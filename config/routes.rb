58e8670edb76ab5c824c19b98ba6fef41a75f955
Hiroaki Nakamura <hnakamur@gmail.com>
2013-11-29 18:28:01 +0900

    Implement 'sign in' and 'sign out' functionalities.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails4GoogleOpenidExample::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  post "/auth/:provider/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
